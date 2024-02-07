66094c56d213f7142adfbe49d9da2ba902031a71
retajal <retaj.shehri@outlook.com>
2024-02-08 00:16:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#homework 1
+#Use Python to solve this equaƟon
+#(x**2+2x-5)
+print((-2+2**2-4*1*-5**0.5) / (2*1))
+print((-2-2**2-4*1*-5**0.5) / (2*1))
+
+#homework2
+#Write a program that computes the area of a rectangle from the length and width.
+print("write the area of a rectangle from the length and width. ")
+lenth = int(input("write the lenth "))
+width = int(input("write the width"))
+area = lenth*width
+print("this is the area of the rectangle" , area)
